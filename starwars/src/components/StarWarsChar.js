56c3c9220a0e130d2405adcaaafe43f06d0d8ead
Josemaria <josemarianriagu25@gmail.com>
2019-05-31 17:07:30 +0800

    Fix DOMSTringNesting issue Change heading levels

--- starwars/src/components/StarWarsChar.js
+++ starwars/src/components/StarWarsChar.js
@@ -7,15 +7,15 @@ const StarWarsChar = props => {
             className="starwars-char"
             key={props.key}
         >
-            <h3>{props.name}</h3>
+            <h2>{props.name}</h2>
             <div className="basic-info">
-                <h4>Basic Info</h4>
+                <h3>Basic Info</h3>
                 <p><span>Birth Year: </span>{props.birthyear}</p>
                 <p><span>Created On: </span>{props.created}</p>
                 <p><span>Edited On: </span>{props.edited}</p>
             </div>
             <div className="bio-details">
-                <h4>Bio Details</h4>
+                <h3>Bio Details</h3>
                 <p><span>Gender: </span>{props.gender}</p>
                 <p><span>Height: </span>{props.height}</p>
                 <p><span>Mass: </span>{props.mass}</p>
@@ -24,19 +24,18 @@ const StarWarsChar = props => {
                 <p><span>Skin Color: </span>{props.skincolor}</p>
             </div>
             <div className="special-info">
-                <h4><span>Movies</span></h4>
-                <p>{props.films.map(film => <p><a href={film} rel="noopener noreferrer" target="_blank">{film}</a></p>)}</p>
+                <h3><span>Movies</span></h3>
+                <div>{props.films.map(film => <p><a href={film} rel="noopener noreferrer" target="_blank">{film}</a></p>)}</div>
             </div>
             <div className="special-info">
-                <h4>Unique Attributes</h4>
-                <p><span>Species: </span>{props.species.map(species => <p><a href={species}>{species}</a></p>)}</p>
-                <p><span>Starships: </span>{props.starships.map(starships => <p><a href={starships}>{starships}</a></p>)}</p>
-                <p><span>Vehicles: </span>{props.vehicles.map(vehicles => <p><a href={vehicles}>{vehicles}</a></p>)}</p>
-
+                <h3>Unique Attributes</h3>
+                <div><span>Species: </span>{props.species.map(species => <p><a href={species}>{species}</a></p>)}</div>
+                <div><span>Starships: </span>{props.starships.map(starships => <p><a href={starships}>{starships}</a></p>)}</div>
+                <div><span>Vehicles: </span>{props.vehicles.map(vehicles => <p><a href={vehicles}>{vehicles}</a></p>)}</div>
             </div>
             <div className="link-info">
-                <h4>Link</h4>
-                F<p><span>Homeworld: </span><a href={props.homeworld}>{props.homeworld}</a></p>
+                <h3>Link</h3>
+                <p><span>Homeworld: </span><a href={props.homeworld}>{props.homeworld}</a></p>
                 <p><span>URL: </span><a href={props.url}>{props.url}</a></p>
             </div>
         </div>
