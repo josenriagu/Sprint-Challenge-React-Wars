03d432615f93e9e0b1709c3bbe54ed8dcd6393e6
Josemaria <josemarianriagu25@gmail.com>
2019-06-01 00:21:52 +0800

    Refactor component to take in all props

--- starwars/src/components/StarWarsCharList.js
+++ starwars/src/components/StarWarsCharList.js
@@ -32,22 +32,7 @@ const StarWarsCharList = props => {
                         return (
                             <StarWars
                                 key={index}
-                                name={char.name}
-                                created={char.created}
-                                edited={char.edited}
-                                birthyear={char.birth_year}
-                                gender={char.gender}
-                                height={char.height}
-                                mass={char.mass}
-                                eyecolor={char.eye_color}
-                                haircolor={char.hair_color}
-                                skincolor={char.skin_color}
-                                homeworld={char.homeworld}
-                                url={char.url}
-                                films={char.films}
-                                species={char.species}
-                                starships={char.starships}
-                                vehicles={char.vehicles}
+                                char={char}
                             />
                         );
                     })
