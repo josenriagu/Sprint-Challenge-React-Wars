c6f04a072554878094241d488390820c38cd05aa
Josemaria <josemarianriagu25@gmail.com>
2019-05-31 17:57:45 +0800

    Define button display style on state change

--- starwars/src/components/StarWarsCharList.js
+++ starwars/src/components/StarWarsCharList.js
@@ -3,37 +3,51 @@ import StarWars from './StarWarsChar';
 import './StarWars.css';
 
 const StarWarsCharList = props => {
+    let styleP = {
+        display: props.previous ? "" : "none"
+    }
+    let styleN = {
+        display: props.next ? "" : "none"
+    }
     return (
-        <div className="starwarschar-list">
-            {/* <div className="buttons">
-                <button className="previous" onClick={props.onClickPrev}>Prev</button>
-                <button className="Next" onClick={props.onClickNext}>Next</button>
-            </div> */}
-            {
-                props.starwarsChars.map((char, index) => {
-                    return (
-                        <StarWars
-                            key={index}
-                            name={char.name}
-                            created={char.created}
-                            edited={char.edited}
-                            birthyear={char.birth_year}
-                            gender={char.gender}
-                            height={char.height}
-                            mass={char.mass}
-                            eyecolor={char.eye_color}
-                            haircolor={char.hair_color}
-                            skincolor={char.skin_color}
-                            homeworld={char.homeworld}
-                            url={char.url}
-                            films={char.films}
-                            species={char.species}
-                            starships={char.starships}
-                            vehicles={char.vehicles}
-                        />
-                    );
-                })
-            }
+        <div className="main">
+            <div className="buttons">
+                <button
+                    className="previous"
+                    style={styleP}
+                    onClick={props.onClickPrev}>Prev</button>
+                <button
+                    className="Next"
+                    style={styleN}
+                    onClick={props.onClickNext}>Next</button>
+            </div>
+            <div className="starwarschar-list">
+                {
+                    props.starwarsChars.map((char, index) => {
+                        return (
+                            <StarWars
+                                key={index}
+                                name={char.name}
+                                created={char.created}
+                                edited={char.edited}
+                                birthyear={char.birth_year}
+                                gender={char.gender}
+                                height={char.height}
+                                mass={char.mass}
+                                eyecolor={char.eye_color}
+                                haircolor={char.hair_color}
+                                skincolor={char.skin_color}
+                                homeworld={char.homeworld}
+                                url={char.url}
+                                films={char.films}
+                                species={char.species}
+                                starships={char.starships}
+                                vehicles={char.vehicles}
+                            />
+                        );
+                    })
+                }
+            </div>
         </div>
     )
 };
